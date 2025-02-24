cc5c04ac6bad571a81a0fcf586329f72ee029745
Niko Iljin <iljinisa@student.jyu.fi>
2025-02-24 11:09:16 +0200

    alustin react frontin uudelleen

--- backend/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import PageContent
-
-class PageContentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = PageContent
-        fields = '__all__'
--- backend/api/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from .models import PageContent
-from .serializers import PageContentSerializer
-
-class PageContentViewSet(viewsets.ModelViewSet):
-    queryset = PageContent.objects.all()
-    serializer_class = PageContentSerializer
