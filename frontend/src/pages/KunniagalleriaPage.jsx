c349279e2bc37122a10ffa0f682c3bd05c8466b0
osterava <olli.s.terava@student.jyu.fi>
2025-03-20 11:19:05 +0200

    kunniagalleria - refraktorointia

--- frontend/src/pages/KunniagalleriaPage.jsx
+++ frontend/src/pages/KunniagalleriaPage.jsx
@@ -1,6 +1,26 @@
 import React from 'react'
 import starter from '../images/Page_starters/12.png'
 import halli2022 from '../images/halli_2022.png'
+import olli from '../images/Olli.png'
+import niko from '../images/Niko.jpg'
+import VuodenAlgolaiset from "../components/vuodenAlgolaiset.jsx";
+import Perustajat
+ from '../components/perustajat.jsx';
+const vuodenAlgolaiset = [
+  { year: 2024, name: "Olli Terävä", image: olli },
+  { year: 2023, name: "Niko Iljin", image: niko }
+]
+
+const perustajat2022 = [
+  { name: "Eemil Hukkanen", role: "Puheenjohtaja" },
+  { name: "Annarella Manninen", role: "Varapuheenjohtaja & Kopo" },
+  { name: "Topias Liljegren", role: "Sihteeri" },
+  { name: "Anna-Sofia Paavonen", role: "Rahastonhoitaja" },
+  { name: "Juuso Vuorela", role: "Yrityssuhdevastaava" },
+  { name: "Minea Nupponen & Jesse Haimi", role: "Tapahtuma- ja somevastaava(t)" },
+  { name: "Lassi Laitinen", role: "Fuksivastaava ja sosiaalipoliittinen vastaava" }
+]
+
 
 const KunniagalleriaPage = () => {
   return (
@@ -8,15 +28,14 @@ const KunniagalleriaPage = () => {
       <img src={starter} alt="starter_image_rules" className="starter" />
       <div className="kunnia-container">
         <h2>Algo ry:n kunniajäsenet</h2>
-
         <p>
           <i>
             Kunniajäsen on killan toimintaa erityisen ansiokkaasti tukenut
             henkilö. Kunniajäsenyys on Algon korkein kunnianosoitus.
           </i>
         </p>
-        <ul>
-          <li>Jari Haapasaari (Nimetty 2023)</li>
+        <ul className='kunniajasenet'>
+          <li>Jari Haapasaari <i>(Nimetty 2023)</i> -- kuva?</li>
         </ul>
         <h2>Vuoden Algolainen - palkinnon voittajat</h2>
         <p>
@@ -26,50 +45,9 @@ const KunniagalleriaPage = () => {
             edistänyt jäsenten välistä yhteishenkeä.
           </i>
         </p>
-        <ul>
-          <div>
-          <li>
-            <strong>2024:</strong> Olli Terävä
-          </li>
-          </div>
-          <div>
-          <li>
-            <strong>2023:</strong> Niko Iljin
-          </li>
-          </div>
-        </ul>
+        <VuodenAlgolaiset people={vuodenAlgolaiset} />
       </div>
-      <div className="kunnia-container">
-        <h2>Perustajajäsenet & Hallitus 2022</h2>
-        <div className="perustaja-flex">
-          <img src={halli2022} alt="halli" width={250} />
-          <ul className="perustaja">
-            <li>
-              <strong>Eemil Hukkanen</strong>, Puheenjohtaja 2022
-            </li>
-            <li>
-              <strong>Annarella Manninen</strong>, Varapuheenjohtaja & Kopo 2022
-            </li>
-            <li>
-              <strong>Topias Liljegren</strong>, Sihteeri 2022
-            </li>
-            <li>
-              <strong>Anna-Sofia Paavonen</strong>, Rahastonhoitaja 2022
-            </li>
-            <li>
-              <strong>Juuso Vuorela</strong>, Yrityssuhdevastaava 2022
-            </li>
-            <li>
-              <strong>Minea Nupponen & Jesse Haimi</strong>, Tapahtuma- ja
-              somevastaava(t) 2022
-            </li>
-            <li>
-              <strong>Lassi Laitinen</strong>, Fuksivastaava ja
-              sosiaalipoliittinen vastaava 2022
-            </li>
-          </ul>
-        </div>
-      </div>
+      <Perustajat image={halli2022} members={perustajat2022} year={2022} />
       <div className="kunnia-container">
         <p>
           Muut edelliset hallitukset näkevät{' '}
